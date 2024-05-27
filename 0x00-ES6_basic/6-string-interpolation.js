b621bae1df80677261d251760c77349ed55cf0c3
Huclark <vhuclark@gmail.com>
2024-05-27 14:57:22 +0000

    Update: task 0

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -8,5 +8,7 @@ export default function getSanFranciscoDescription() {
 
   const { income, gdp, capita } = budget;
 
-  return `As of ${year}$, it was the seventh-highest income county in the United States, with a per capita personal income of ${income}. As of 2015, San Francisco proper had a GDP of ${gdp}, and a GDP per capita of ${capita}.`;
+  return 'As of ' + year + ', it was the seventh-highest income county in the United States'
+  / ', with a per capita personal income of ' + income + '. As of 2015, San Francisco'
+  / ' proper had a GDP of ' + gdp + ', and a GDP per capita of ' + capita + '.';
 }
