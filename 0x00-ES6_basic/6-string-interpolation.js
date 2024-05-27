c85b7cf16ae3f39a975bdde2bda0c2462bc82f89
Huclark <vhuclark@gmail.com>
2024-05-27 15:00:09 +0000

    Fix: template

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,9 +6,5 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  const { income, gdp, capita } = budget;
-
-  return 'As of ' + year + ', it was the seventh-highest income county in the United States'
-  / ', with a per capita personal income of ' + income + '. As of 2015, San Francisco'
-  / ' proper had a GDP of ' + gdp + ', and a GDP per capita of ' + capita + '.';
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
