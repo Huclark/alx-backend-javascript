fafa6c9f6ba74a2705c48037bcd6908e4dc143b6
Huclark <vhuclark@gmail.com>
2024-05-27 23:06:49 +0000

    Add: task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+
+  for (const value of array) {
+    result.push(appendString + value);
+  }
+
+  return result;
+}
