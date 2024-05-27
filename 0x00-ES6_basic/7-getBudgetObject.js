769cc90029c46931f9ea3237a93fbeb268248eb6
Huclark <vhuclark@gmail.com>
2024-05-27 15:07:44 +0000

    Fix: task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,12 +1,3 @@
-export default function getSanFranciscoDescription() {
-  const year = 2017;
-  const budget = {
-    income: '$119,868',
-    gdp: '$154.2 billion',
-    capita: '$178,479',
-  };
-
-  const {income, gdp, capita} = budget
-
-  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${gdp}, and a GDP per capita of ${capita}.`;
+export default function getBudgetObject(income, gdp, capita) {
+  return { income, gdp, capita };
 }
