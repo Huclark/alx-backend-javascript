c714722afc3d5ffc927c30aecea2361f69480394
Huclark <vhuclark@gmail.com>
2024-05-29 18:02:47 +0000

    Add: task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const result = [];
+      values.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          result.push({
+            status: element.status,
+            value: element.value,
+          });
+        } else {
+          result.push({
+            status: element.status,
+            value: `${element.reason}`,
+          });
+        }
+      });
+      return result;
+    });
+}
