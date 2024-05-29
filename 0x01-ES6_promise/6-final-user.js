ed5c642048f0ddae08c839e60f32876018ab0d39
Huclark <vhuclark@gmail.com>
2024-05-29 18:03:51 +0000

    Fix: linting

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises)
     .then((values) => {
       const result = [];
